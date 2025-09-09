d1b3dec352d6438e4ef5a57410ea31b3405171c1
EdwinMakRouts <makoveevedwin@gmail.com>
2025-09-09 10:20:14 +0200

    making little by little responsive

--- src/app/pages/filter-leagues/filter-leagues.ts
+++ src/app/pages/filter-leagues/filter-leagues.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LeaguesService } from '../../core/services/leagues.service';
+import { LoadedDataService } from '../../core/services/loaded-data.service';
+import { League } from '../../core/models/league/league';
+import { CommonModule } from '@angular/common';
+import { CardTeam } from '../../shared/components/card-team/card-team';
 
 @Component({
   selector: 'app-filter-leagues',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardTeam],
   templateUrl: './filter-leagues.html',
-  styleUrl: './filter-leagues.css'
+  styleUrls: ['./filter-leagues.css'],
 })
-export class FilterLeagues {
+export class FilterLeagues implements OnInit {
+  isLoading = true;
+  leagues: League[] = [];
+  colors = ['#0284c7', '#16a34a', '#ea580c', '#dc2626', '#9333ea'];
 
+  constructor(
+    private router: Router,
+    private leaguesService: LeaguesService,
+    private loadedDataService: LoadedDataService
+  ) {}
+
+  ngOnInit() {
+    this.leagues = this.loadedDataService.getLeagues();
+
+    if (this.leagues.length == 0) {
+      this.leaguesService.getLeagues().subscribe((leagues: League[]) => {
+        this.leagues = leagues;
+        this.loadedDataService.setLeagues(leagues);
+      });
+    }
+
+    this.isLoading = false;
+  }
+
+  handleLeagueSelected(id: String) {
+    this.router.navigate(['/filter-clubs'], { queryParams: { league_id: id } });
+  }
 }
--- src/app/pages/general/general.ts
+++ src/app/pages/general/general.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CardTeam } from '../../shared/components/card-team/card-team';
@@ -8,7 +8,7 @@ import { CountriesService } from '../../core/services/countries.service';
 import { Country } from '../../core/models/country/country';
 import { Club } from '../../core/models/club/club';
 import { League } from '../../core/models/league/league';
-import { LoadedDataService } from '../../core/services/loaded-data';
+import { LoadedDataService } from '../../core/services/loaded-data.service';
 
 @Component({
   selector: 'app-general',
@@ -17,7 +17,7 @@ import { LoadedDataService } from '../../core/services/loaded-data';
   templateUrl: './general.html',
   styleUrls: ['./general.css'],
 })
-export class General {
+export class General implements OnInit {
   isLoading: boolean = true;
   countries: Country[] = [];
   clubs: Club[] = [];
@@ -30,21 +30,38 @@ export class General {
     private leagueservice: LeaguesService,
     private countryservice: CountriesService,
     private loadedDataService: LoadedDataService
-  ) {
-    this.countryservice.getCountries().subscribe((countries: Country[]) => {
-      this.countries = countries;
-      this.loadedDataService.setCountries(countries);
-    });
-    this.clubservice.getClubs().subscribe((clubs: Club[]) => {
-      this.clubs = clubs;
-      this.loadedDataService.setClubs(clubs);
-    });
-    this.leagueservice.getLeagues().subscribe((leagues: League[]) => {
-      this.leagues = leagues;
-      this.loadedDataService.setLeagues(leagues);
-    });
+  ) {}
 
+  ngOnInit() {
+    this.loadData();
     this.isLoading = false;
+  }
+
+  loadData() {
+    this.countries = this.loadedDataService.getCountries();
+    this.clubs = this.loadedDataService.getClubs();
+    this.leagues = this.loadedDataService.getLeagues();
+
+    if (this.countries.length == 0) {
+      this.countryservice.getCountries().subscribe((countries: Country[]) => {
+        this.countries = countries;
+        this.loadedDataService.setCountries(countries);
+      });
+    }
+
+    if (this.clubs.length == 0) {
+      this.clubservice.getClubs().subscribe((clubs: Club[]) => {
+        this.clubs = clubs;
+        this.loadedDataService.setClubs(clubs);
+      });
+    }
+
+    if (this.leagues.length == 0) {
+      this.leagueservice.getLeagues().subscribe((leagues: League[]) => {
+        this.leagues = leagues;
+        this.loadedDataService.setLeagues(leagues);
+      });
+    }
   }
 
   navigateToPlayer() {
