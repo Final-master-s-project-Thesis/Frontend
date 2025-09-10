f9385faaa07fb556ce6ad73593567a2e7ded5dac
EdwinMakRouts <makoveevedwin@gmail.com>
2025-09-10 23:07:54 +0200

    las commit

--- src/app/core/base/repository.repository.ts
+++ src/app/core/base/repository.repository.ts
@@ -4,31 +4,33 @@ import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 export abstract class Repository {
-    private paths = Object.freeze({
-        localhost: 'http://127.0.0.1:8000',
-    });
+  private paths = Object.freeze({
+    localhost: 'http://127.0.0.1:8000',
+    prod: 'https://backend-k6io.onrender.com',
+  });
 
-    protected basePath = this.paths.localhost;
-    protected http: HttpClient;
+  protected basePath = this.paths.prod;
+  protected http: HttpClient;
 
-    constructor(protected injector: Injector) {
-        this.http = this.injector.get(HttpClient);
+  constructor(protected injector: Injector) {
+    this.http = this.injector.get(HttpClient);
+  }
+
+  protected doRequest<T>(
+    method: keyof HttpClient,
+    url: string,
+    options?: { params?: any; body?: any }
+  ): Observable<T> {
+    // Si es GET o HEAD, no enviar body
+    const opts: any = { ...options };
+    if (method === 'get' || method === 'head') {
+      delete opts.body;
     }
-
-    protected doRequest<T>(
-        method: keyof HttpClient,
-        url: string,
-        options?: { params?: any; body?: any }
-    ): Observable<T> {
-        // Si es GET o HEAD, no enviar body
-        const opts: any = { ...options };
-        if (method === 'get' || method === 'head') {
-            delete opts.body;
-        }
-        return this.http.request<T>(method, `${this.basePath}${url}`, { ...opts, observe: 'response' })
-            .pipe(
-                filter((event: any) => event instanceof HttpResponse),
-                map((event: HttpResponse<T>) => event.body as T)
-            );
-    }
+    return this.http
+      .request<T>(method, `${this.basePath}${url}`, { ...opts, observe: 'response' })
+      .pipe(
+        filter((event: any) => event instanceof HttpResponse),
+        map((event: HttpResponse<T>) => event.body as T)
+      );
+  }
 }
